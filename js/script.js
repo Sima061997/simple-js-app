4bcacef1570f132d48db542441f60b6a2ad8be00
Sima Dhimal <sima.dhimal97@gmail.com>
2022-01-20 11:34:02 +0100

    Pokemon added

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let pokemonTypes = ['electric', ['grass', 'poison'], 'fire'];
+let pokemonList = [
+               {name: 'Pikachu', height: 40},        //height in cm
+                {name: 'Ivysaur', height: 100},       //height in cm
+              {name: 'Charmander', height: 60},     //height in cm
+                pokemonTypes
+                ];
+
