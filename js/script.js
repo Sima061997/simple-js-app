fcef861bd8bf9674be31580c513b2c6ed00de185
Sima Dhimal <sima.dhimal97@gmail.com>
2022-01-20 18:04:40 +0100

    Features of Pokemen added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
+let height;
 let pokemonTypes = ['electric', ['grass', 'poison'], 'fire'];
 let pokemonList = [
-               {name: 'Pikachu', height: 40},        //height in cm
-                {name: 'Ivysaur', height: 100},       //height in cm
-              {name: 'Charmander', height: 60},     //height in cm
-                pokemonTypes
+              {name: 'Pikachu',  height: 40},                 //height in cm
+              {name: 'Ivysaur', height: 100},                //height in cm
+              {name: 'Charmander', height: 60},                  //height in cm
+                pokemonTypes               
                 ];
 
+       for(let i = 0; i < pokemonList.length -1; i++) { 
+         if (pokemonList[i].height > 80 ){       
+            document.write( pokemonList[i].name + ' (' + 'height:'+ pokemonList[i].height + ')'+   ' -Woww!! It\'s Big' + "<br>")
+         }
+         else {
+            document.write( pokemonList[i].name + ' (' + 'height:'+ pokemonList[i].height + ')' + "<br>")
+         }
+       }
+       
