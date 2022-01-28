339608e5c393d000543c1890712379af94d15ca7
Sima Dhimal <sima.dhimal97@gmail.com>
2022-01-28 22:48:55 +0100

    querySelector and eventListener Commited

--- js/script.js
+++ js/script.js
@@ -1,34 +1,62 @@
-      let pokemonRepository = (function () {
+
+let pokemonRepository = (function () {
          let pokemonList = [
-            {name: 'Pikachu',  height: 4,  type: 'electric'},                 //height in m
-            {name: 'Ivysaur', height: 10,  type: ['grass', ' poison']},            
-            {name: 'Charmander', height: 6, type: 'fire'}  
+            {name: 'Pikachu',  height: 0.4,  type: 'electric'},                 //height in m
+            {name: 'Ivysaur', height: 1,  type: ['grass', ' poison']},            
+            {name: 'Charmander', height: 0.6, type: 'fire'}  
      ];
       
        function add(pokemon){
           pokemonList.push(pokemon);
        }
-        
+         
        function getAll(){
            return pokemonList;
        }
+
+      function addListItem(pokemon){
+         //element 'ul' is selected and 'li', 'button' elements are created
+
+         let variable = document.querySelector('ul');
+         let listItem = document.createElement('li');   
+         let button = document.createElement('button');        
+         button.innerText = pokemon.name;
+
+         //class added
+
+         button.classList.add('class-to-button');
+         listItem.appendChild(button);
+         variable.appendChild(listItem);
+
+        button.addEventListener('click', function (pokemon){
+         showDetails(pokemon);
+        } ) 
+      }
+       //shows the details of Pokemon clicked
+
+      function showDetails(pokemon){
+         console.log(pokemon);
+      }
       
       return {
          add: add,
-         getAll: getAll
+         getAll: getAll,
+         addListItem: addListItem,
+         showDetails: showDetails
       };
       }) ();
-      
-      let nameOfPokemon = pokemonRepository.getAll().forEach(function (pokemon) {
-         console.log(pokemon.name, "List in My Pokemon");
-      })
-      
-      
+      //a Pokemon added on the given array
+
+      pokemonRepository.add({ name: 'Butterfree', height: 1.1, type: ['Bug', 'Flying'] });
       console.log(pokemonRepository.getAll());
-      pokemonRepository.add({ name: 'Butterfree', height: 11, type: ['Bug', 'Flying'] });
       
       function addv (){
        if( typeof(pokemonRepository.name) === String  || typeof(pokemonRepository.height) === Number ) {
          return add();      
          }
       }
+
+      let nameOfPokemon = pokemonRepository.getAll().forEach(function (pokemon) {
+         
+         pokemonRepository.addListItem(pokemon);
+      })
